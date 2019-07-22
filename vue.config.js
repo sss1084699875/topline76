ebd0a36312445b94dbb75b2eae616657fa55937d
sss1084699875 <1622925998@qq.com>
2019-07-22 10:22:27 +0800

    初始化文件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: true,
+  devServer: {// 实时保存、编译的配置段
+    open: true, // 自动开启浏览器
+    port: 3000, // 服务运行端口（小于65000的基本都可以，前提是其他进程没有使用）
+    host: '127.0.0.1' // 运行服务的主机名(是电脑的真实ip，不能乱定义)
+  }
 }
